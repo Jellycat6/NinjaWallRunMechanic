a48ac4151084eea512318387d91d6214e2b7aa87
Jalyn <jle210001@utdallas.edu>
2022-04-10 22:00:36 -0500

    Fixed Movement and Added Jump

--- Assets/Scripts/ThirdPersonMovement.cs
+++ Assets/Scripts/ThirdPersonMovement.cs
@@ -10,10 +10,25 @@ public class ThirdPersonMovement : MonoBehaviour
     [SerializeField] float _speed = 6f;
     [SerializeField] float _turnSmoothTime = 0.1f;
     [SerializeField] float _turnSmoothVelocity;
+    [SerializeField] float _gravity = -9.8f;
+    [SerializeField] float _jumpHeight = 3f;
+    [SerializeField] Transform _groundCheck;
+    [SerializeField] float _groundDistance = 0.4f;
+    [SerializeField] LayerMask _groundMask;
+    [SerializeField] bool _isGrounded;
+    Vector3 _velocity;
 
-    // Update is called once per frame
+    private void Start()
+    {
+            Cursor.lockState = CursorLockMode.Locked;
+    }
     void Update()
     {
+        _isGrounded = Physics.CheckSphere(_groundCheck.position, _groundDistance, _groundMask);
+        if (_isGrounded && _velocity.y < 0)
+        {
+            _velocity.y = -2f;
+        }
         float horizontal = Input.GetAxisRaw("Horizontal");
         float vertical = Input.GetAxisRaw("Vertical");
 
@@ -21,13 +36,22 @@ public class ThirdPersonMovement : MonoBehaviour
 
         if(direction.magnitude >= 0.1f)
         {
-            float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg * cam.eulerAngles.y;
+            float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
             float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref _turnSmoothVelocity, _turnSmoothTime);
             transform.rotation = Quaternion.Euler(0f, angle, 0f);
 
             Vector3 moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
             _controller.Move(moveDir.normalized * _speed * Time.deltaTime);
         }
+        if (Input.GetKeyDown(KeyCode.Space) && _isGrounded)
+        {
+            _velocity.y = Mathf.Sqrt(_jumpHeight * -2f * _gravity);
+            Debug.Log("Jump");
+        }
+
+        _velocity.y += _gravity * Time.deltaTime;
+
+        _controller.Move(_velocity * Time.deltaTime);
 
     }
 }

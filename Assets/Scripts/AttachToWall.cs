cbb440963c19ffe4604b65267cfc8cba84cb41ed
Jalyn <jle210001@utdallas.edu>
2022-04-11 10:07:07 -0500

    Added Colliders For Climbing Along With Script

---
+++ Assets/Scripts/AttachToWall.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AttachToWall : MonoBehaviour
+{
+    [SerializeField] GameObject _stayAttatchedToWall;
+    [SerializeField] GameObject _player;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("Collision");
+        if (other.tag == "Climable")
+        {
+            Debug.Log("Climb");
+
+            AttatchToWall();
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void AttatchToWall()
+    {
+        _player.transform.Rotate(-90, 0, 0);
+        _stayAttatchedToWall.SetActive(true);
+    }
+}
